fcfac1e5bebcf56002fce89941759bb3469d45b1
João Pedro <joao.pedreira@estudante.ufjf.br>
2023-07-01 19:57:19 -0300

    Set the cliente socket as a nonblocking socket

--- client/providers/UDPClient.py
+++ client/providers/UDPClient.py
@@ -3,8 +3,7 @@ import json
 from sys import path
 path.append('..')
 
-from entities.Package import Package
-
+from trabalho_redes.entities.Package import Package
 
 class UDPClient:
     def __init__(self, server_addess="127.0.0.1", server_port=20001, buffer_size=1024):
@@ -12,6 +11,7 @@ class UDPClient:
         self.server_port = server_port
         self.buffer_size = buffer_size
         self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.socket.setblocking(0)
 
     def send_package(self, package: Package) -> None:
         package_dump = package.to_json_str()
@@ -23,7 +23,6 @@ class UDPClient:
         response_encoded, address = self.socket.recvfrom(self.buffer_size)
         response_decoded = response_encoded.decode()
         response = json.loads(response_decoded)
-        
         return response
 
     def listen(self):
@@ -33,8 +32,9 @@ class UDPClient:
             # self.send_package(package2)
 
             # Recebe responses (ACKs)
-
-            response = self.receive()
-
+            try:
+                response = self.receive()
+            except BlockingIOError:
+                continue
             # Reajusta a janela de acordo com os ACKs recebidos
             
