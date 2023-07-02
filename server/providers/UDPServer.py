c8c21bf19b51b1625eac7d068934eb5ac7691536
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2023-07-02 16:34:51 -0300

    Feat: Adds ACK to the server.
    Co-authored-by: Miguel Sales <salesmiguelz@users.noreply.github.com>
    Co-authored-by: mariadornelas <mariadornelas@users.noreply.github.com>

--- server/providers/UDPServer.py
+++ server/providers/UDPServer.py
@@ -14,16 +14,36 @@ class UDPServer:
         print("Servidor UDP up e escutando...")
 
     def listen(self):
+
+        expected_sequence_number = 0
+        received_data = []
+
         while True:
             try: 
                 request = self.receive_request()
                 print("Mensagem do Cliente: {}".format(request))
+                # Verificar se o pacote recebido é o esperado
+                if request["sequence_number"] == expected_sequence_number:
+                    # Processamento (ou só salvar pacote no buffer)
+                    received_data.append(request["body"])
+                    # Incrementa número de sequência esperado
+                    expected_sequence_number += 1
+                    # Enviar response (ACK)
+                    self.reply_with_ack(self, expected_sequence_number)
+                    continue
+                else:
+                    # Enviar response (ACK) com número anterior (indica que o pacote foi recebido, mas não é o esperado)
+                    self.reply_with_ack(self, expected_sequence_number)
+                    continue
+
             except BlockingIOError: 
                 continue
-            # Processamento (ou só salvar pacote no buffer...)
-            
-            # Enviar response (ACK)
 
+    def reply_with_ack(self, sequence_number):
+        # Monta o pacote de resposta, no caso, converte ACK para string e depois para bytes
+        package_encoded = (json.dumps({"sequence_number": sequence_number})).encode()
+        # Envia o pacote de resposta
+        self.socket.sendto(package_encoded, self.client_address)
 
     def receive_request(self):
         request_encoded, address = self.socket.recvfrom(self.buffer_size)
@@ -33,7 +53,7 @@ class UDPServer:
         request = json.loads(request_decoded)
 
         return request
-            
+
     def response(self, body: dict):
         response_body = json.dumps(body)
         response_body_encoded = str.encode(response_body)
