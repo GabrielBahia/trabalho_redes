fd15173bf71dcdd4aeb7641be5b7586949b7de45
Miguel Sales <miguelsales02@gmail.com>
2023-07-12 17:29:34 -0300

    Cria package buffer no server e arq de teste

--- server/providers/UDPServer.py
+++ server/providers/UDPServer.py
@@ -12,8 +12,8 @@ class UDPServer:
         self.local_port = local_port
         self.buffer_size = buffer_size
         self.window_size = 10
+        self.window_start = 0
         self.packages_buffer = PriorityQueue(self.window_size)
-
         self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.socket.bind((self.local_ip, self.local_port))
         self.socket.setblocking(0)
@@ -24,7 +24,7 @@ class UDPServer:
 
         while True:
             try:
-                # DropTail (descarta novos pacotes)
+                # Checa se fila do buffer está lotada: DropTail (descarta novos pacotes)
                 if self.packages_buffer.qsize() == self.window_size: 
                     continue
 
@@ -42,12 +42,12 @@ class UDPServer:
 
                 # Verificar se o pacote recebido é o esperado
                 if request.sequence_number == expected_sequence_number:
-                    # Processamento (ou só salvar pacote no buffer) // Fila de prioridade
+                    # Processamento (salva pacote no buffer (uso de fila de prioridade para ordenar de acordo com número de sequência))
                     self.packages_buffer.put((request.sequence_number, request))
 
                     # Enviar response (ACK)
                     self.reply_with_ack(expected_sequence_number)
-                    
+
                     # Incrementa número de sequência esperado
                     expected_sequence_number += 1
 
@@ -55,8 +55,8 @@ class UDPServer:
                     if random.random() <= 0.3:
                         if not self.packages_buffer.empty():
                             yield self.packages_buffer.get()[1]
-
                     continue
+
                 else:
                     # Enviar response (ACK) com número anterior (indica que o pacote foi recebido, mas não é o esperado)
                     self.reply_with_ack(expected_sequence_number)
